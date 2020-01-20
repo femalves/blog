03be69465f9250d9bcac4ecff2d0c86a97841e7e
Fernanda Alves <fernanda__m@hotmail.com>
2020-01-19 23:17:35 -0300

    forms

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -3,11 +3,12 @@ from .models import Comment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
+    name = forms.CharField(
+        max_length=25, widget=forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Name'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Email', 'type': 'email'}))
+    to = forms.EmailField(widget=forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Email to', 'type': 'email'}))
     comments = forms.CharField(required=False,
-                               widget=forms.Textarea)
+                               widget=forms.Textarea(attrs={'class': 'uk-textarea', 'placeholder': 'Comment'}))
 
 
 class CommentForm(forms.ModelForm):
@@ -15,7 +16,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Name'}),
+            'email': forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Email', 'type': 'email'}),
+            'body': forms.Textarea(attrs={'class': 'uk-textarea', 'placeholder': 'Comment'}),
+        }
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField()
+    query = forms.CharField(widget=forms.TextInput(attrs={'class': 'search'}))
