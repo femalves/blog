9cd07a713c63a3a9419ad8a67fa12f643f6e45fb
Fernanda Alves <fernanda__m@hotmail.com>
2020-02-14 20:43:20 -0300

    removed items

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -5,8 +5,10 @@ from .models import Comment
 class EmailPostForm(forms.Form):
     name = forms.CharField(
         max_length=25, widget=forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Name'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Email', 'type': 'email'}))
-    to = forms.EmailField(widget=forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Email to', 'type': 'email'}))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={'class': 'uk-input', 'placeholder': 'Email', 'type': 'email'}))
+    to = forms.EmailField(widget=forms.TextInput(
+        attrs={'class': 'uk-input', 'placeholder': 'Email to', 'type': 'email'}))
     comments = forms.CharField(required=False,
                                widget=forms.Textarea(attrs={'class': 'uk-textarea', 'placeholder': 'Comment'}))
 
@@ -21,7 +23,3 @@ class CommentForm(forms.ModelForm):
             'email': forms.TextInput(attrs={'class': 'uk-input', 'placeholder': 'Email', 'type': 'email'}),
             'body': forms.Textarea(attrs={'class': 'uk-textarea', 'placeholder': 'Comment'}),
         }
-
-
-class SearchForm(forms.Form):
-    query = forms.CharField(widget=forms.TextInput(attrs={'class': 'search'}))
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
-from .forms import EmailPostForm, CommentForm, SearchForm
+from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
@@ -79,20 +78,3 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
-
-
-def post_search(request):
-
-    form = SearchForm()
-    query = None
-    results = []
-    if 'query' in request.GET:
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            query = form.cleaned_data['query']
-            # search_vector = SearchVector(
-            #     'title', weigh='A') + SearchVector('body', weight='B')
-            # search_query = SearchQuery(query)
-            results = Post.objects.annotate(
-                similarity=TrigramSimilarity('title', query),).filter(similarity__gt=0.3).order_by('-similarity')
-    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
